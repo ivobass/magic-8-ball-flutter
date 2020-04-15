6e9768d9002452d1628310a3ab5f30ee69730d66
Ivo Baptista <ivobass@gmail.com>
2020-04-15 12:03:17 +0100

    Ask me Anything

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Ballapp(),
       ),
     );
+
+class Ballapp extends StatefulWidget {
+  Ballapp({Key key}) : super(key: key);
+
+  @override
+  _BallappState createState() => _BallappState();
+}
+
+class _BallappState extends State<Ballapp> {
+  int answer = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything',
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),),
+      ),
+      body: Container(
+        color: Colors.blue[400],
+        child: Center(
+          child: GestureDetector(
+            onTap: (){
+              setState(() {
+                answer = Random().nextInt(5)+1;
+              });
+            },
+                      child: Container(
+              
+              child: Image.asset('images/ball$answer.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
